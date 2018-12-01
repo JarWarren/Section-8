06384ad1f8ee62abf3bf0d715b56bfbf420eb77e
Arkin Hill <ahill@luminpublishing.com>
2018-12-01 13:59:10 -0700

    Step 5 text data entered

--- Section 8/Models/Item.swift
+++ Section 8/Models/Item.swift
@@ -14,13 +14,15 @@ class Item {
     let text: String?
     let buttonText: String?
     let url: String?
+    let graphicName: String?
     let format: ItemFormat
     
-    init(title: String?, text: String?, buttonText: String?, url: String?, format: ItemFormat) {
+    init(title: String?, text: String?, buttonText: String?, url: String?, graphicName: String?, format: ItemFormat) {
         self.title = title
         self.text = text
         self.buttonText = buttonText
         self.url = url
+        self.graphicName = graphicName
         self.format = format
     }
 }
