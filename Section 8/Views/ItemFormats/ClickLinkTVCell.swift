25b678146b57b03d734b1432bee00c3450f42157
Arkin Hill <ahill@luminpublishing.com>
2018-12-01 11:01:41 -0700

    Hi

---
+++ Section 8/Views/ItemFormats/ClickLinkTVCell.swift
@@ -0,0 +1,31 @@
+//
+//  ClickLinkTVCell.swift
+//  Section 8
+//
+//  Created by Arkin Hill on 11/30/18.
+//  Copyright © 2018 Warren. All rights reserved.
+//
+
+import UIKit
+
+class ClickLinkTVCell: UITableViewCell {
+
+    // MARK: - OUTLETS
+    
+    @IBOutlet weak var clickLinkTextLabel: UILabel!
+    @IBOutlet weak var clickLinkButtonText: UIButton!
+    
+    // MARK: - ACTIONS
+    
+    @IBAction func clickLinkButtonTapped(sender: AnyObject) {
+        if let url = URL(string: "https://www.waitlistcheck.com/application/form.php?ID=972-UT011") {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:])
+            }
+        }
+    }
+    
+    
+    
+
+}
