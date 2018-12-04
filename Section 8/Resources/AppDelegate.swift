290af48f103b83e59720b38bd9309e579219b12d
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2018-12-03 21:05:43 -0700

    Contains added code in the final step button tapped that will ensure all pending and delievered notification will removed. Also, contains code in the app delegate to remove badge icon from the app's icon

--- Section 8/Resources/AppDelegate.swift
+++ Section 8/Resources/AppDelegate.swift
@@ -70,6 +70,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        application.applicationIconBadgeNumber = 0;
+
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
